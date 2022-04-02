06d0e830ac63244a5da4921da81fc88cffb90014
Gonzalo Araoz <FabienAraoz@outlook.com>
2022-04-02 09:19:22 -0500

    comenzamos a ver la POO en js

--- archivo_ejemplo.js
+++
@@ -1,3 +0,0 @@
-//comenzamos con el documento en js.
-
-var sustitucion = getElementById("sas");
---
+++ index.js
@@ -0,0 +1,25 @@
+
+var nombre;
+var edad;
+var valor;
+var guapo;
+
+class Persona{
+     constructor(nombre,edad,valor,guapo){
+         this.nombre = nombre;
+         this.edad = edad;
+         this.valor = valor;
+         this.guapo = guapo;
+     }
+     //mçetodos
+     interpretar(){
+        console.log(this.info());
+     }
+     info(){
+         return ("Nombre=> "+this.nombre+", edad = "+this.edad+", valor = "+this.valor+", es guapo?: "+this.guapo+".");
+     }
+}
+
+const Araoz = new Persona("Gonzalo", 18, true, true);
+Araoz.interpretar();
+//esperemos que esto funcione :)
